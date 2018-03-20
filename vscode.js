8869374a7c455fe45da3b272ac4f92432901291b
John Milazzo <github@johnmilazzo.com>
2018-03-19 22:51:15 -0500

    Create vscode.js

---
+++ vscode.js
@@ -0,0 +1,30 @@
+{
+  "editor.cursorBlinking": "solid",
+  "editor.detectIndentation": false,
+  "editor.fontFamily": "'Operator Mono', Inconsolata, Consolas, 'Courier New', monospace",
+  "editor.fontSize": 19,
+  "editor.formatOnSave": true,
+  "editor.renderLineHighlight": "all",
+  "editor.renderWhitespace": "all",
+  "editor.rulers": [
+    80,
+    100,
+    120
+  ],
+  "editor.selectionHighlight": false,
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "emmet.triggerExpansionOnTab": true,
+  "files.eol": "\n",
+  "files.trimTrailingWhitespace": true,
+  "prettier.parser": "flow",
+  "prettier.printWidth": 120,
+  "prettier.singleQuote": true,
+  "terminal.integrated.shell.windows": "C:\\WINDOWS\\System32\\bash.exe",
+  "vscode_custom_css.imports": [
+    "file:///c:/Users/john.milazzo/.vscodestyles.css"
+  ],
+  "vsicons.dontShowNewVersionMessage": true,
+  "window.title": "${activeEditorLong}${separator}${dirty}",
+  "workbench.colorTheme": "Cobalt2",
+}
