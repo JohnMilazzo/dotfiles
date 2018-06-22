f045efe53d7ee59346afed5c025404022a589852
John Milazzo <github@johnmilazzo.com>
2018-06-22 15:30:52 -0500

    Create vscode-mac.js

---
+++ vscode-mac.js
@@ -0,0 +1,43 @@
+{
+  "editor.cursorBlinking": "solid",
+  "editor.detectIndentation": false,
+  "editor.fontFamily": "'Operator Mono', Inconsolata, Consolas, 'Courier New', monospace",
+  "editor.fontSize": 19,
+  "editor.renderLineHighlight": "all",
+  "editor.renderWhitespace": "all",
+  "editor.rulers": [
+    80,
+    100,
+    120
+  ],
+  "editor.selectionHighlight": false,
+  "editor.tabSize": 2,
+  "editor.wordWrap": "on",
+  "emmet.triggerExpansionOnTab": true,
+  "files.eol": "\n",
+  "files.trimTrailingWhitespace": true,
+  "prettier.disableLanguages": [
+    "js"
+  ],
+  // Very important: Install this plugin: https://github.com/be5invis/vscode-custom-css
+  // You'll need to change this to a file URI scheme on your computer: https://en.wikipedia.org/wiki/File_URI_scheme
+  // Mac/Linux: file:///Users/YOUR-PC-USERNAME/.vscodestyles.css
+  // Windows: file:///C:/Users/YOUR-PC-USERNAME/.vscodestyles.css
+  "vscode_custom_css.imports": [
+    "file:///Users/johnmilazzo/.vscodestyles.css"
+  ],
+  "vsicons.dontShowNewVersionMessage": true,
+  "telemetry.enableCrashReporter": false,
+  "telemetry.enableTelemetry": false,
+  "window.title": "${activeEditorLong}${separator}${dirty}",
+  "workbench.colorTheme": "Cobalt2",
+  "gitlens.advanced.messages": {
+    "suppressShowKeyBindingsNotice": true
+  },
+  "editor.formatOnSave": true,
+  "eslint.autoFixOnSave": true,
+  "eslint.alwaysShowStatus": true,
+  "files.defaultLanguage": "markdown",
+  "gitlens.historyExplorer.enabled": true,
+  "window.zoomLevel": 0
+}
