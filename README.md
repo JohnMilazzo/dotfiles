eea3561cdffaba3ed5a0679a0edee9a1f4a7c7da
John Milazzo <github@johnmilazzo.com>
2020-05-28 11:23:27 -0500

    late nite

--- README.md
+++ README.md
@@ -1,9 +1,18 @@
 # dotfiles
 
-Hey John!  What settings do you use for _____?
+Hey John! What settings do you use for **\_\_\_\_\_**?
 
-Here are some of the configuration files I use for my editor, apps, linting, debugging, etc.
+Here are most of the configuration files I use for my dev environment, editor, apps, linting, debugging, etc.
 
-## Enjoy!!
-v2020.02.14-2
+Also included are scripts I use for bootstrapping my environment, currently Windows 10's WSL2. (🔥 highly recommended)
 
+## Installation
+
+Open a terminal and follow along!
+
+1. Clone this repository: git clone https://github.com/johnmilazzo/dotfiles
+2. CD into the folder: cd dotfiles
+3. Execute the bootstrap script: sh ./bootstrap.sh
+4. Restart the terminal or run source ~/.zshrc
+
+👍
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,25 +1,60 @@
 #!/usr/bin/env bash
 
+# start by installing zsh, oh-my-zsh, and related themes, etc.
+
+
+# install zsh
+install_zsh () {
+# Test to see if zsh is installed.  If it is:
+if [ -f /bin/zsh -o -f /usr/bin/zsh ]; then
+  # Set the default shell to zsh if it isn't currently set to zsh
+  if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+      chsh -s $(which zsh)
+  fi
+else
+  sudo apt install zsh
+  install_zsh
+fi
+}
+install_zsh
+
+
+# install oh-my-zsh
+cd ~
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+
+# install PowerLevel10K theme:
+git clone https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+
+# now let's customize our config files
+
+# make sure we're in the directory of this file
 cd "$(dirname "${BASH_SOURCE}")";
 
+# pull down the latest version of this repo
 git pull origin master;
 
-function doIt() {
+# function to sync the files from the repo to the home directory
+function syncFiles() {
 	rsync --exclude ".git/" \
 		--exclude "bootstrap.sh" \
 		--exclude "README.md" \
 		--exclude "LICENSE-MIT.txt" \
 		-avh --no-perms . ~;
-	source ~/.bash_profile;
+	source ~/.zshrc;
 }
 
+# warn user about possibly overwriting existing files unless they used the force flag
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt;
+	syncFiles;
 else
 	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1;
 	echo "";
 	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt;
+		syncFiles;
 	fi;
 fi;
-unset doIt;
+
+unset syncFiles;
